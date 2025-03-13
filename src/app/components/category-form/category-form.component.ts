20b0691dceb4697216b4fe65385b7b7a114da951
Roussel-030 <yvanroussel31@gmail.com>
2025-03-13 18:22:30 +0300

    CRUD category

--- src/app/components/category-form/category-form.component.ts
+++ src/app/components/category-form/category-form.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { CategoryRequest } from "./../../types/Category";
+import { Component, inject, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { menuNames } from "../../util/menuNames";
+import { FormsModule } from "@angular/forms";
+import { CategoryService } from "../../services/category/category.service";
 
 @Component({
-  selector: 'app-category-form',
-  imports: [],
-  templateUrl: './category-form.component.html',
-  styleUrl: './category-form.component.css'
+  selector: "app-category-form",
+  imports: [FormsModule],
+  templateUrl: "./category-form.component.html",
+  styleUrl: "./category-form.component.css",
 })
-export class CategoryFormComponent {
+export class CategoryFormComponent implements OnInit {
+  isAddForm: boolean = false;
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+  categoryService = inject(CategoryService);
+  categoryRequest: CategoryRequest = { name: "" };
 
+  ngOnInit(): void {
+    this.isAddForm = this.router.url.includes("add");
+    if (!this.isAddForm) {
+      this.getCategoryById();
+    }
+  }
+
+  goToCategory() {
+    this.router.navigate([menuNames.category.path]);
+  }
+
+  getCategoryById() {
+    const categoryId: number | null = Number(
+      this.route.snapshot.paramMap.get("id")
+    );
+    if (categoryId) {
+      this.categoryService
+        .getByIdCategory(categoryId)
+        .subscribe((category) => (this.categoryRequest = category));
+    }
+  }
+
+  onSubmit() {
+    if (this.isAddForm) {
+      this.categoryService
+        .createCategory(this.categoryRequest)
+        .subscribe(() => this.goToCategory());
+    } else {
+      this.categoryService
+        .updateCategory(this.categoryRequest)
+        .subscribe(() => this.goToCategory());
+    }
+  }
 }
--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,6 +1,10 @@
+import { CategoryRequest } from "./../../types/Category";
 import { Component, inject, OnInit } from "@angular/core";
-import { CategoryRequest } from "../../types/Category";
+
 import { CategoryService } from "../../services/category/category.service";
+
+import { menuNames } from "../../util/menuNames";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-category-list",
@@ -10,11 +14,34 @@ import { CategoryService } from "../../services/category/category.service";
 })
 export class CategoryListComponent implements OnInit {
   categoryList: CategoryRequest[] = [];
+  router = inject(Router);
   categoryService = inject(CategoryService);
 
   ngOnInit(): void {
     this.categoryService
       .readCategory()
       .subscribe((categoryList) => (this.categoryList = categoryList.data));
+  }
+
+  deleteCategory(categoryRequest: CategoryRequest) {
+    if (categoryRequest.id) {
+      this.categoryService
+        .deleteCategory(categoryRequest.id)
+        .subscribe(() => console.log("Success"));
+    }
+  }
+
+  goToAddCategory() {
+    this.router.navigate([menuNames.category.path, "add"]);
+  }
+
+  goToEditCategory(categoryRequest: CategoryRequest) {
+    if (categoryRequest.id) {
+      this.router.navigate([
+        menuNames.category.path,
+        "edit",
+        categoryRequest.id,
+      ]);
+    }
   }
 }
