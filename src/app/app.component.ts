b8dc3ef60ae21225afe1217ee728d6ab543279d1
Roussel-030 <yvanroussel31@gmail.com>
2025-03-13 09:59:27 +0300

    Fix header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminHeaderComponent } from "./components/admin-header/admin-header.component";
+import { UserHeaderComponent } from "./components/user-header/user-header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminHeaderComponent, UserHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
