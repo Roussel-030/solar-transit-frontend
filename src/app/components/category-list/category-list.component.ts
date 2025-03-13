58adfc76411b15cc0a86e01101a7a722ddfcc814
Roussel-030 <yvanroussel31@gmail.com>
2025-03-13 15:14:40 +0300

    Fix category-list-component

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from "@angular/core";
+import { CategoryRequest } from "../../types/Category";
+import { CategoryService } from "../../services/category/category.service";
 
 @Component({
-  selector: 'app-category-list',
+  selector: "app-category-list",
   imports: [],
-  templateUrl: './category-list.component.html',
-  styleUrl: './category-list.component.css'
+  templateUrl: "./category-list.component.html",
+  styleUrl: "./category-list.component.css",
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+  categoryList: CategoryRequest[] = [];
+  categoryService = inject(CategoryService);
 
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
 }
