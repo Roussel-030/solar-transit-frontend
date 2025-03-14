e9c177a257f67b17c93e09222c0beb0696d3d265
Roussel-030 <yvanroussel31@gmail.com>
2025-03-14 10:07:06 +0300

    Fix design map.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { AdminHeaderComponent } from "./components/admin-header/admin-header.component";
-import { UserHeaderComponent } from "./components/user-header/user-header.component";
 import { Role } from "./types/Role";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./components/header/header/header.component";
