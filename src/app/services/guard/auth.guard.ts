64884eed5477388bee8bd08ae076312b422de4b5
Roussel-030 <yvanroussel31@gmail.com>
2025-03-19 14:51:36 +0300

    Fix auth.guard

--- src/app/services/guard/auth.guard.ts
+++ src/app/services/guard/auth.guard.ts
@@ -3,10 +3,11 @@ import {
   Router,
   CanActivate,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot
+  RouterStateSnapshot,
 } from "@angular/router";
 import { AuthenticationService } from "../authentication/authentication.service";
 import { RegisterRequest } from "../../types/Register";
+import { menuNames } from "../../util/menuNames";
 
 @Injectable({ providedIn: "root" })
 export class AuthGuard implements CanActivate {
@@ -26,7 +27,9 @@ export class AuthGuard implements CanActivate {
     this.authService.verify().subscribe();
 
     // not logged in so redirect to login page with the return url
-    this.router.navigate(["/login"], { queryParams: { returnUrl: state.url } });
+    this.router.navigate([menuNames.login.path], {
+      queryParams: { returnUrl: state.url },
+    });
     return false;
   }
 }
